b7f5a174bb3c882af8682f12638c31c52b835f64
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-02-07 12:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let tmp="0123456789abcdefghijklmnopqrstuvwxyz";
+	let str="";
+	for(let i=0;i<l;i++){
+		let index=Math.floor(Math.random()*(tmp.length-1));
+		str+=tmp[index];
+	}
+	return str;
 }
 
 // Do not change the code below.
